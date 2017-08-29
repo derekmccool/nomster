1d4f425d82d83227eddd829f4b1d61fb27d12f8b
Derek <you@example.com>
2017-08-29 07:05:45 +0000

    added edit page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -18,7 +18,11 @@ class PlacesController < ApplicationController
     def show
       @place = Place.find(params[:id])
     end
-    
+
+    def edit
+      @place = Place.find(params[:id])
+    end
+
     private
     
     def place_params
