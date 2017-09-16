9c75ddcc9587dc3f7edf2495655b3e9261f79b89
Derek <you@example.com>
2017-09-16 03:28:55 +0000

    added  comment form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -21,6 +21,7 @@ class PlacesController < ApplicationController
 
     def show
       @place = Place.find(params[:id])
+      @comment = Comment.new
     end
 
     def edit
@@ -56,7 +57,7 @@ class PlacesController < ApplicationController
     end
 
     private
-    
+
     def place_params
       params.require(:place).permit(:name, :description, :address)
     end
